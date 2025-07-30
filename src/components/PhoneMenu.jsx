b1ce191ad5fca91ed79797ef6696a86475e6036c
Caleb Adebayo <calebtolorunleke@yahoo.com>
2025-07-29 21:52:30 -0700

    update on travel legnos landing page

--- src/components/PhoneMenu.jsx
+++ src/components/PhoneMenu.jsx
@@ -3,7 +3,12 @@ import React from "react";
 const PhoneMenu = () => {
   return (
     <div className="fixed top-0 left-0 w-screen h-screen bg-blue-600 flex items-center justify-center z-40">
-      <h1 className="text-white text-3xl">Phone Menu</h1>
+      <ul className="flex flex-col gap-10 text-white text-3xl uppercase">
+        <li>Phone Menu</li>
+        <li>Cities & Regions</li>
+        <li>Plan ahead</li>
+        <li>where to stay</li>
+      </ul>
     </div>
   );
 };
